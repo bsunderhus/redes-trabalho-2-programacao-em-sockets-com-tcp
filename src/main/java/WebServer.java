58befe7040ce6dc7955b90b3e7031a7381389c00
Bernardo Lucas Almeida Sunderhus <bernardo.sunderhus@gmail.com>
2017-12-24 15:10:44 -0200

    Adicionando aqruivos html e de recursos

--- src/main/java/WebServer.java
+++ src/main/java/WebServer.java
@@ -3,45 +3,112 @@ import java.net.*;
 import java.util.*;
 
 public class WebServer {
-    public static void main(String args[]) throws Exception {
-        String requestMessageLine;
-        String fileName;
-        ServerSocket listenSocket = new ServerSocket(3000);
+    private static int PORT = 3000;
+    private static void respondSocket(Socket connectionSocket) throws IOException{
+        System.out.println("Starting to process request...");
+        BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
+        DataOutputStream outToClient = new DataOutputStream(connectionSocket.getOutputStream());
+        String firstLine = inFromClient.readLine();
+        if (firstLine == null) return;
+        StringTokenizer tokenizedLine = new StringTokenizer(firstLine);
+        String method = tokenizedLine.nextToken().toUpperCase();
+        String path = tokenizedLine.nextToken();
+        String protocol = tokenizedLine.nextToken();
+        if (!protocol.startsWith("HTTP")) {
+            System.out.println("Not an HTTP Request");
+            return;
+        }
+        System.out.println(protocol + " " + method + " Request:");
+        System.out.println("  " + firstLine);
+        String restOfLines;
+        while ((restOfLines = inFromClient.readLine()).length() > 0) System.out.println("  " + restOfLines);
+        if (method.equals("GET")) writeFile(path, outToClient, false);
+        else if (method.equals("HEAD")) writeFile(path, outToClient, true);
+        else writeBadRequest(outToClient);
+        connectionSocket.close();
+    }
 
-        System.out.println("Web server listening to port 3000...");
-        Socket connectionSocket = listenSocket.accept();
-        System.out.println("Starting to proccess request...");
-        BufferedReader inFromClient = new BufferedReader(new
-                InputStreamReader(connectionSocket.getInputStream()));
-        DataOutputStream outToClient = new
-                DataOutputStream(connectionSocket.getOutputStream());
-        requestMessageLine = inFromClient.readLine();
-        StringTokenizer tokenizedLine = new StringTokenizer(requestMessageLine);
-
-        if (tokenizedLine.nextToken().equals("GET")) {
-            fileName = tokenizedLine.nextToken();
-            if (fileName.startsWith("/") == true) {
-                fileName = fileName.substring(1);
-            }
-            System.out.println("Request filename= "+fileName);
-            File file = new File(fileName);
+    private static void writeFile (String fileName, DataOutputStream outToClient, boolean isHead) throws IOException {
+        if (fileName.startsWith("/")) fileName = fileName.substring(1);
+        if (fileName.length() == 0) fileName = "index.html";
+        if (isHead) System.out.println("HTTP/1.0 HEAD Response:");
+        else System.out.println("HTTP/1.0 GET Response:");
+        try {
+            URL url = WebServer.class.getClassLoader().getResource(fileName);
+            String filePath = url.getPath();
+            File file = new File(filePath);
             int numOfBytes = (int) file.length();
-            FileInputStream inFile = new FileInputStream(fileName);
             byte[] fileInBytes = new byte[numOfBytes];
+            FileInputStream inFile = new FileInputStream(filePath);
             inFile.read(fileInBytes);
-            outToClient.writeBytes("HTTP/1.0 200 Document Follows\r\n");
+            String output = "HTTP/1.0 200 Document Follows\r\n";
+            outToClient.writeBytes(output);
+            System.out.print("  " + output);
             if (fileName.endsWith("jpg")) {
-                outToClient.writeBytes("Content-Type: image/jpeg\r\n");
+                output = "Content-Type: image/jpeg\r\n";
+                outToClient.writeBytes(output);
+                System.out.print("  " + output);
             }
             if (fileName.endsWith("gif")) {
-                outToClient.writeBytes("Content-Type: image/gif\r\n");
+                output = "Content-Type: image/gif\r\n";
+                outToClient.writeBytes(output);
+                System.out.print("  " + output);
             }
+            if (fileName.endsWith("txt")) {
+                output = "Content-Type: text/plain\r\n";
+                outToClient.writeBytes(output);
+                System.out.print("  " + output);
+            }
+            System.out.print("  Content-length: " + numOfBytes + "\r\n");
             outToClient.writeBytes("Content-length: " + numOfBytes + "\r\n");
-            outToClient.writeBytes("\r\n"); // fim de cabeçalho
-            outToClient.write(fileInBytes, 0, numOfBytes);
-            connectionSocket.close();
-        } else {
-            System.out.println("Bad Request Message");
+            System.out.print("  \r\n");
+            outToClient.writeBytes("\r\n");
+            if (!isHead) outToClient.write(fileInBytes, 0, numOfBytes);
+        } catch (Exception e) {
+            String output = "HTTP/1.0 404 Not Found\r\n";
+            outToClient.writeBytes(output);
+            System.out.print("  " + output);
+            System.out.print("  \r\n");
+            outToClient.writeBytes("\r\n");
+        }
+    }
+
+    private static void writeBadRequest (DataOutputStream outToClient) throws  IOException {
+        System.out.println("HTTP/1.0 Bad Request Response:");
+        System.out.print("  HTTP/1.0 400 Bad Request\r\n");
+        outToClient.writeBytes("HTTP/1.0 400 Bad Request\r\n");
+        System.out.print("  Content-length: " + 0 + "\r\n");
+        outToClient.writeBytes("Content-length: " + 0 + "\r\n");
+        System.out.print("  \r\n");
+        outToClient.writeBytes("\r\n");
+    }
+
+    public static void main(String args[]) throws IOException {
+        ServerSocket listenSocket = new ServerSocket(PORT);
+        System.out.println("Web server listening to port 3000...");
+        int aux = 1;
+        while (true) {
+            Socket socket = listenSocket.accept();
+            System.out.println("Creating Thread to attend request");
+            new RespondThread(socket, aux++).start();
+        }
+    }
+    static class RespondThread extends Thread {
+        Socket socket;
+        int id;
+        public RespondThread(Socket socket, int id) {
+            System.out.println("Thread " + id + " created successfully");
+            this.socket = socket;
+            this.id = id;
+        }
+        public void run() {
+            System.out.println("Starting Thread " + id);
+            try {
+                WebServer.respondSocket(socket);
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            System.out.println("Stopping Thread " + id);
         }
     }
 }
