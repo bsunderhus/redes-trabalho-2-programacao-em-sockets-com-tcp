bb3886801e84612875afd48415899aa59a6f6eb9
Bernardo Lucas Almeida Sunderhus <bernardo.sunderhus@gmail.com>
2017-12-26 22:14:59 -0200

    ajustes finais

--- src/main/java/br/inf/ufes/redes/WebServer.java
+++ src/main/java/br/inf/ufes/redes/WebServer.java
@@ -6,6 +6,13 @@ import java.util.*;
 
 public class WebServer {
     private static int PORT = 3000;
+
+    /**
+     * Método utilizado para processamento de conexão socket. Tem como objetivo verificar se a mensagem vindo pela conexão realmentre está no formato de uma mensagem do protocolo HTTP.
+     * O método processa a primeira linha da mensagem para verificar se o método requerido pelo protocolo HTTP é GET ou HEAD, logo em seguida invoca funções para tratamento e envio de mensagem para os casos de GET, HEAd e de Bad Requests.
+     * @param connectionSocket conexão Socket a ser verificada e respondida
+     * @throws IOException lança exceção caso algum arquivo inexistente tente ser processado ou caso falhe na escrita da Stream de retorno do Socket
+     */
     private static void respondSocket(Socket connectionSocket) throws IOException{
         System.out.println("Starting to process request...");
         BufferedReader inFromClient = new BufferedReader(new InputStreamReader(connectionSocket.getInputStream()));
@@ -18,31 +25,33 @@ public class WebServer {
         String protocol = tokenizedLine.nextToken();
         if (!protocol.startsWith("HTTP")) {
             System.out.println("Not an HTTP Request");
+            connectionSocket.close();
             return;
         }
         System.out.println(protocol + " " + method + " Request:");
         System.out.println("  " + firstLine);
         String restOfLines;
         while ((restOfLines = inFromClient.readLine()).length() > 0) System.out.println("  " + restOfLines);
-        if (method.equals("GET")) writeFile(path, outToClient, false);
-        else if (method.equals("HEAD")) writeFile(path, outToClient, true);
+        if (method.equals("GET")) writeMessage(path, outToClient, false);
+        else if (method.equals("HEAD")) writeMessage(path, outToClient, true);
         else writeBadRequest(outToClient);
         connectionSocket.close();
     }
 
-    private static void writeFile (String fileName, DataOutputStream outToClient, boolean isHead) throws IOException {
+    /**
+     * Método utilizado para escrever a mensagem de retorono para a conexão socket.
+     * Responsável por escrever o cabeçalho e o corpo da mensagem, gerando erro de arquivo não encontrado caso tente acessar um arquivo inexistente.
+     * @param fileName Nome do arquivo desejado
+     * @param outToClient Stream de resposta ao cliente
+     * @param isHead booleano declarando se a mensagem é GET ou HEAD
+     * @throws IOException lança exceção caso algum arquivo inexistente tente ser processado.
+     */
+    private static void writeMessage(String fileName, DataOutputStream outToClient, boolean isHead) throws IOException {
         if (fileName.startsWith("/")) fileName = fileName.substring(1);
         if (fileName.length() == 0) fileName = "index.html";
         if (isHead) System.out.println("HTTP/1.0 HEAD Response:");
         else System.out.println("HTTP/1.0 GET Response:");
         try {
-//            URL url = WebServer.class.getResource("/" + fileName);
-            // System.out.println("File path: " +filePath);
-//            File file = new File(url.toURI());
-//            int numOfBytes = (int) file.length();
-//            byte[] fileInBytes = new byte[numOfBytes];
-//            FileInputStream inFile = new FileInputStream(file);
-//            inFile.read(fileInBytes);
             System.out.println(WebServer.class.getResource("/" + fileName).getPath());
             InputStream fileInputStream = WebServer.class.getResourceAsStream("/" + fileName);
             int read;
@@ -110,6 +119,11 @@ public class WebServer {
         }
     }
 
+    /**
+     * Método utilizado para escrever a mensagem de Bad Request para a conexão socket.
+     * @param outToClient Stream de saída da mensagem para o Socket
+     * @throws IOException lança exceçao caso falhe ao escrever na stream do Socket
+     */
     private static void writeBadRequest (DataOutputStream outToClient) throws  IOException {
         System.out.println("HTTP/1.0 Bad Request Response:");
         System.out.print("  HTTP/1.0 400 Bad Request\r\n");
@@ -132,6 +146,10 @@ public class WebServer {
             currentThread.start();
         }
     }
+
+    /**
+     * Thread utilizada para processamento de mensagens em um socket
+     */
     static class RespondThread extends Thread {
         Socket socket;
         int id;
